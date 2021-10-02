ec1e771640ad9c33cc78f0299bb913452f8a9dba
sydney <sydneychase5841@gmail.com>
2021-10-02 10:46:36 -0400

    updated blopost controller w/error messages, record not found, recordinvalid

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,4 +1,6 @@
 class BlogpostsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 
      # GET /blogposts
@@ -14,6 +16,18 @@ class BlogpostsController < ApplicationController
     
     end
 
+
+
+    private
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+      end
     
+      def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+
+
 
 end
