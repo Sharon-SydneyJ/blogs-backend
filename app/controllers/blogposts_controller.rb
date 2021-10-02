f068a42af06b0f0c9b199fe448cc32cb325ebae1
sydney <sydneychase5841@gmail.com>
2021-10-02 10:08:11 -0400

    blogpost index action added and tested in postman properly

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,2 +1,10 @@
 class BlogpostsController < ApplicationController
+
+
+     # GET /blogposts
+  def index
+    blogposts = Blogpost.all.order(content: :desc)
+    render json: blogposts, include: :comments
+  end
+
 end
