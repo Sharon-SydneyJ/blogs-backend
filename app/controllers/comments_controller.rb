01597e87937a65ebcddb30404fb4246d2c30dd0b
sydney <sydneychase5841@gmail.com>
2021-10-02 10:34:59 -0400

    comments show action added tested in postman working properly

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+
+
+        def show
+        comment = Comment.find_by(id: params[:id])
+          render json: comment, include: [:blogpost, :user]
+      end
 end
