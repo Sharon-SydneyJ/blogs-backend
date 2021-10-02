03174fb1177654cefb180a341abcab48cf7b4919
sydney <sydneychase5841@gmail.com>
2021-10-02 09:25:06 -0400

    added json error messages, record not found and record invalid.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-   
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     
     
     # GET /users
@@ -35,7 +36,14 @@ class UsersController < ApplicationController
   end
 
   private
+  
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
   def user_params
     params.permit(:first_name, :last_name, :user_name, :email, :userid, :blogpost)
