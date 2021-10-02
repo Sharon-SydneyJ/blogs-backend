7a4dc31ca67ab8fba77634199836e696fa5470b8
sydney <sydneychase5841@gmail.com>
2021-10-02 11:36:09 -0400

    added comment update and destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,29 @@
 class CommentsController < ApplicationController
 
-
+    # GET /comments/:id
         def show
         comment = Comment.find_by(id: params[:id])
           render json: comment, include: [:blogpost, :user]
       end
 
-      # Post/users/:id
+      # Post/comment/:id
     def create
         comment = Comment.create(comment_params)
         render json: comment, status: :created
       end
+
+       # PUT /comments/:id
+    def update
+        comment = Comment.find_by(id: params[:id])
+        comment.update(comment_params)
+        render json: comment
+      end
+
+      # DELETE /comments/:id
+    def destroy
+    comment = Comment.find_by(id: params[:id])
+    comment.destroy
+    head :no_content
+    end
 
 end
