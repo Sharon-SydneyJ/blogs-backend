433f1946c2e936c8b60908f7bf0d4eeb31155001
sydney <sydneychase5841@gmail.com>
2021-10-10 03:19:02 -0400

    no changes

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ resources :users do
 end
   resources :users, only: [:index, :show, :create, :update, :destroy]
 
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
