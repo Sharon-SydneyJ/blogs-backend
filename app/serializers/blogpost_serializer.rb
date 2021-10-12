6d00b52be799aeb34415bee063364b50e356f22b
sydney <sydneychase5841@gmail.com>
2021-10-12 16:19:10 -0400

    added blogpost_comment serializer

--- app/serializers/blogpost_serializer.rb
+++ app/serializers/blogpost_serializer.rb
@@ -2,6 +2,7 @@ class BlogpostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :img_url
 
    belongs_to :user
-   has_many :comments
+   has_many :comments, serializer: BlogpostCommentSerializer
+   
   
 end
