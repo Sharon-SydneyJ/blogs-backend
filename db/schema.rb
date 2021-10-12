fa9adcc4b65776850494846eafaa95bc1728f0b4
sydney <sydneychase5841@gmail.com>
2021-10-12 16:00:42 -0400

    schema modified due to table column name change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_122651) do
+ActiveRecord::Schema.define(version: 2021_10_11_014415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_10_02_122651) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
+    t.text "remark"
     t.bigint "user_id", null: false
     t.bigint "blogpost_id", null: false
     t.datetime "created_at", precision: 6, null: false
