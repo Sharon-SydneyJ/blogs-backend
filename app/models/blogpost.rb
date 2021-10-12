adfd6c05aee7d253213cf489a3a0e10cdaa5408f
sydney <sydneychase5841@gmail.com>
2021-10-12 16:22:50 -0400

    added accepts nested attributes for comments

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -3,4 +3,5 @@ class Blogpost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments
 end
