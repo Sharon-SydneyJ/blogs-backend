b8258b6ad0b13269b2a75ca78f9f4b6a2e33b26c
sydney <sydneychase5841@gmail.com>
2021-10-14 19:52:01 -0400

    updated syntax error in user_blogpost serializer and added Profile

--- app/serializers/user_blogpost_serializer.rb
+++ app/serializers/user_blogpost_serializer.rb
@@ -1,6 +1,6 @@
 class UserBlogpostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :img_url
 
-  has_many :comments, 
+  has_many :comments 
   has_many :blogposts, serializer: BlogpostUserSerializer
 end
